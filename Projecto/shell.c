2abd7458d15eaf03a11701a29a3e1477bb62c4ba
zecarlos94 <zecarlos@MBP-de-Jose-4.home>
2015-05-23 18:28:10 +0100

    Na união dos dois da seg.fault 11

--- Projecto/shell.c
+++ Projecto/shell.c
@@ -7,7 +7,79 @@
 #define START 1000
 
 Vector vector;/*Variável global*/
-  
+
+ssize_t readln(int file, char *buf, size_t nbyte){ 
+	int i = 0; int n; 
+	do{ 
+		n = read (file, &buf[i], 1); i++; 
+	}while ( n>0 && (buf[i-1] != '\n') && i<nbyte); 
+	buf[i-1]=0; 
+	if (n>0) return i;
+    else return -1; 
+}
+
+int cloudShell(char* cmd){
+	int n=0;char* a[1000];
+	a[n]=strtok(cmd, " ");
+	while(a[n]) a[++n]=strtok(NULL, " ");
+	//strtok(0," \n");
+	
+	if(!fork()){
+		if(!strcmp(a[0],"cd")){chdir(a[1]);}
+		else{
+		execvp(a[0],a);
+		exit(1);
+		}
+	}
+	else{
+		wait(NULL);
+	}
+	return 0;	
+}
+
+void menu() {
+        printf ("\n\n--------------------------------MENU----------------------------------------\n\n");
+        printf ("1 - LOGIN\n");
+        printf ("2 - REGISTAR\n");
+        printf ("----------------------------------------------------------------------------\n\n");
+  }
+
+void login(Vector vector){
+  int i;  
+  char* nome,password;
+  printf ("ESCREVA O SEU NOME DE UTILIZADOR\n");
+  if(scanf("%s", &nome));
+  printf ("ESCREVA A SUA PASSWORD\n");
+  if(scanf("%s", &password));
+  /*Para cada i corresponde a informação nos arrays do mesmo utilizador*/
+  for(i=0;i<tamanho;i++){/*Dá segmentation fault*/
+    if((strcmp(vector_getNomes(&vector,i),nome)) && (strcmp(vector_getPasswords(&vector,i),&password)) ){
+      printf("BEM VINDO %s !!!\n", nome);
+      /*onde vai executar a cloudShell*/
+     	char* cmd;
+		char buf[1000];
+		while(1){
+			write(1,"cloudShell $ ",14);
+			readln(0,buf,1000);
+			cloudShell(buf);
+        	if(buf[0]=='S' && buf[1]=='\0') break;
+		}
+    }
+  }
+}
+
+void registar(Vector vector){
+  char* nome,password;
+  int saldo=START;
+  printf ("ESCREVA O SEU NOME DE UTILIZADOR\n");
+  if(scanf("%s", &nome))
+  printf ("ESCREVA A SUA PASSWORD\n");
+  if(scanf("%s", &password))
+  vector_append(&vector, nome, password, saldo);
+  printf("%d\n", tamanho);
+}
+
+
 int main(){
   int opcao;
   vector_init(&vector);/*vector.size==0*/
@@ -28,78 +100,4 @@ int main(){
   }
   vector_free(&vector);
   return 0;
-}
-
-void menu () {
-        printf ("\n\n--------------------------------MENU----------------------------------------\n\n");
-        printf ("1 - LOGIN\n");
-        printf ("2 - REGISTAR\n");
-        printf ("----------------------------------------------------------------------------\n\n");
-  }
-
-void login(Vector vector){
-  int i;  
-  char* nome,password;
-  printf ("ESCREVA O SEU NOME DE UTILIZADOR\n");
-  if(scanf("%s", &nome));
-  printf ("ESCREVA A SUA PASSWORD\n");
-  if(scanf("%s", &password));
-  /*Para cada i corresponde a informação nos arrays do mesmo utilizador*/
-  for(i=0;i<tamanho;i++){/*Dá segmentation fault*/
-    if((strcmp(vector_getNomes(&vector,i),nome)) && (strcmp(vector_getPasswords(&vector,i),password)) ){
-      printf("BEM VINDO %s !!!\n", nome);
-      /*onde vai executar a cloudShell*/
-      char* cmd;
-      char buf[1000];
-      char c;
-      while(1){
-        write(1,"CloudShell $ ",13);
-        readln(0,buf,1000);
-        if(buf[0]=='S'&& buf[1]=='\0') break;
-        cloudShell(buf);
-      }
-
-    }
-  }
-}
-
-void registar(Vector vector){
-  char* nome,password;
-  int saldo=START;
-  printf ("ESCREVA O SEU NOME DE UTILIZADOR\n");
-  if(scanf("%s", &nome))
-  printf ("ESCREVA A SUA PASSWORD\n");
-  if(scanf("%s", &password))
-  vector_append(&vector, nome, password, saldo);
-  printf("%d\n", tamanho);
-}
-
-
-ssize_t readln(int file, char *buf, size_t nbyte){ 
-  int i = 0; int n; 
-  do{ 
-    n = read (file, &buf[i], 1); i++; 
-  }while ( n>0 && (buf[i-1] != '\n') && i<nbyte); 
-  buf[i-1]=0; 
-  if (n>0) return i;
-    else return -1; 
-}
-
-int cloudShell(char* cmd){
-  int n=0;char* a[1000];
-  a[n]=strtok(cmd, " ");
-  while(a[n]) a[++n]=strtok(NULL, " ");
-  //strtok(0," \n");
-  
-  if(!fork()){
-    if(!strcmp(a[0],"cd")){chdir(a[1]);}
-    else{
-    execvp(a[0],a);
-    exit(1);
-    }
-  }
-  else{
-    wait(NULL);
-  }
-  return 0; 
 }
